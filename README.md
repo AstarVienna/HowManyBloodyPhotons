66f17c7d7c96682c8a876e71e2036038db55bb1d
Kieran <kdleschinski@gmail.com>
2021-11-26 15:40:15 +0100

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # How Many Bloody Photons
 Tiny repo to calculate astronomical photon counts for various instruments
+
+### Install
+
+    pip install git+https://github.com/AstarVienna/HowManyBloodyPhotons.git 
+
+
+### Basic usage
+
+```python
+>>> from astropy import units as u
+>>> import hmbp
+```
+
+Find the number of photons emitted by Vega in V band [ph s-1 m-2]::
+         
+```python
+>>> hmbp.in_vega_spectrum("V")
+```
+
+Find the number of photons emitted by a Ks=20 [ABmag] point source through
+the HAWKI Ks filter::
+    
+```python
+>>> hmbp.for_magnitude_in_filter("Ks", 20*u.ABmag, 
+                                 observatory="Paranal", 
+                                 instrument="HAWKI")
+```
--- hmbp/main.py
+++ hmbp/main.py
@@ -26,6 +26,23 @@ def for_magnitude_in_filter(filter_name, magnitude,
     n_ph : expected photon count in the filter
         [ph s-1 m-2 (arcsec-2)]
 
+    Examples
+    --------
+    ::
+        >>> from astropy import units as u
+        >>> import hmbp
+
+    Find the number of photons emitted by Vega in V band [ph s-1 m-2]::
+
+        >>> hmbp.in_vega_spectrum("V")
+
+    Find the number of photons emitted by a Ks=20 [ABmag] point source through
+    the HAWKI Ks filter::
+
+        >>> hmbp.for_magnitude_in_filter("Ks", 20*u.ABmag,
+                                         observatory="Paranal",
+                                         instrument="HAWKI")
+
     """
 
     if observatory is None and instrument is None and \
