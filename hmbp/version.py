f2e28df6dc6a0b89bd609a84af8f85b08bd309a9
Kieran <kdleschinski@gmail.com>
2022-11-11 14:14:39 +0100

    minor

--- hmbp/version.py
+++ hmbp/version.py
@@ -1,2 +1,10 @@
-version = '0.2'
-date    = '2021-11-29 13:00:00 GMT'
+version = '0.2.1'
+date    = '2022-11-11 14:00:00 GMT'
+changes = """
+version : '0.2.1'
+date : '2022-11-11'
+author : Kieran Leschinski
+notes:
+- reverted to source_templates from spectrum_templates in hmbp/main.py
+
+"""
